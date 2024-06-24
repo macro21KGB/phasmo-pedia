9e43cf3db1f822ab57d581cc2a9cd6fe9f77f045
Anawil Karavek <akaravek@hotmail.com>
2024-06-24 12:53:02 +0700

    enabled cors

--- api/main.py
+++ api/main.py
@@ -1,9 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from rag_app.query_rag import query_rag, QueryResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SubmitQueryRequest(BaseModel):
     query_text: str
